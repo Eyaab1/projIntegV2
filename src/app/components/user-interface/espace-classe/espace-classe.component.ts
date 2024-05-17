0bffd5daa341be184793236ca6d368db69135c85
abdellaoui Eya <abdellaeya@gmail.com>
2024-05-17 11:52:58 +0100

    nt new

--- src/app/components/user-interface/espace-classe/espace-classe.component.ts
+++ src/app/components/user-interface/espace-classe/espace-classe.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { CalendrierComponent } from "../../calendrier/calendrier.component";
 
 @Component({
-  selector: 'app-espace-classe',
-  standalone: true,
-  imports: [],
-  templateUrl: './espace-classe.component.html',
-  styleUrl: './espace-classe.component.css'
+    selector: 'app-espace-classe',
+    standalone: true,
+    templateUrl: './espace-classe.component.html',
+    styleUrl: './espace-classe.component.css',
+    imports: [CalendrierComponent]
 })
 export class EspaceClasseComponent {
-
+isEnseignat=false;
 }
