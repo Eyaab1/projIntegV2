6deccc776ae0d980d0a7f0ceec434efbb2e36ef2
aramdahmeni <148446627+aramdahmeni@users.noreply.github.com>
2024-05-17 11:50:57 +0100

    updated login/routes/sidebar/fyp

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,45 +1,50 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from 'express';
-import { AuthService } from '../../services/auth.service';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';  // Corrected import
+import { CommonModule } from '@angular/common';
+
+
+
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent  {
-  // loginform!: FormGroup;
+export class LoginComponent implements OnInit  {
+  loginform!: FormGroup;
 
-  // constructor(private router: Router, private fb: FormBuilder, private authservice: AuthService) {}
+  constructor(private router: Router, private fb: FormBuilder) {}
 
+  ngOnInit(): void {
+    this.loginform = this.fb.group({
+      user: ['', Validators.required],
+      pwd: ['', Validators.required]
+    });
+  }
+
+  get pwdmb() {
+    return this.loginform.get('pwd');
+  }
+
+  pwdoblig() {
+    return this.pwdmb?.errors?.['required'] && this.pwdmb?.touched;
+  }
+
+  get usermb() {
+    return this.loginform.get('user');
+  }
+
+  useroblig() {
+    return this.usermb?.errors?.['required'] && this.usermb?.touched;
+  }
   
-
-  // get pwdmb() {
-  //   return this.loginform.get('pwd');
-  // }
-
-  // pwdoblig() {
-  //   return this.pwdmb?.errors?.['required'] && this.pwdmb?.touched;
-  // }
-
-  // get usermb() {
-  //   return this.loginform.get('user');
-  // }
-
-  // useroblig() {
-  //   return this.usermb?.errors?.['required'] && this.usermb?.touched;
-  // }
-  
-  //  login() {
-  //    if (this.loginform.valid) {
-  //      const username = this.loginform.get('user')?.value;
-  //     const password = this.loginform.get('pwd')?.value;
-  // //     this.authservice.login(username, password);     
-
-  //    }
-  //  }
+  login() {
+    if (this.loginform.valid) {
+      const username = this.loginform.get('user')?.value;
+      const password = this.loginform.get('pwd')?.value;
+    }
+  }
   
 }
